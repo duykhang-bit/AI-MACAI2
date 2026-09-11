c051421eee86ffddc9f1e3de4d645c210595af48
duykhang-bit <67537095+duykhang-bit@users.noreply.github.com>
2026-09-11 17:45:23 +0700

    fix clear SKU input toàn bộ uat/PromotionRsaOffline - 20 files: dùng JS clear + CTRL+A + DELETE

--- .kiro/Automation selenium java/Selenium-java-master/src/test/java/testcases/uat/PromotionRsaOffline/TC18.java
+++ .kiro/Automation selenium java/Selenium-java-master/src/test/java/testcases/uat/PromotionRsaOffline/TC18.java
@@ -358,13 +358,11 @@ public class TC18 extends BaseTest1 {
         js.executeScript("arguments[0].click(); arguments[0].focus();", productInput);
         Thread.sleep(500);
 
-        // Clear ô search (Windows: CTRL+A rồi DELETE)
-        js.executeScript(
-                "var el = arguments[0];" +
-                "var nativeSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;" +
-                "nativeSetter.call(el, '');" +
-                "el.dispatchEvent(new Event('input', { bubbles: true }));" +
-                "el.dispatchEvent(new Event('change', { bubbles: true }));", productInput);
+        // Clear ô search trước khi nhập mới
+        js.executeScript("arguments[0].value = '';", productInput);
+        productInput.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        Thread.sleep(200);
+        productInput.sendKeys(Keys.DELETE);
         Thread.sleep(300);
 
         // Nhập mã sản phẩm
--- .kiro/Automation selenium java/Selenium-java-master/src/test/java/testcases/uat/PromotionRsaOffline/TC22.java
+++ .kiro/Automation selenium java/Selenium-java-master/src/test/java/testcases/uat/PromotionRsaOffline/TC22.java
@@ -212,12 +212,11 @@ public class TC22 extends BaseTest1 {
         WebElement productInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[starts-with(@id,'search-product-input_session')]")));
         js.executeScript("arguments[0].click(); arguments[0].focus();", productInput);
         Thread.sleep(500);
-        js.executeScript(
-                "var el = arguments[0];" +
-                "var nativeSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;" +
-                "nativeSetter.call(el, '');" +
-                "el.dispatchEvent(new Event('input', { bubbles: true }));" +
-                "el.dispatchEvent(new Event('change', { bubbles: true }));", productInput);
+        // Clear ô search trước khi nhập mới
+        js.executeScript("arguments[0].value = '';", productInput);
+        productInput.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        Thread.sleep(200);
+        productInput.sendKeys(Keys.DELETE);
         Thread.sleep(300);
         productInput.sendKeys(productCode);
         Thread.sleep(1000);
